4adbe4c86c3ebd29557ccf3bcc0fb90d696c69a5
charles-16 <charles.j16@gmail.com>
2023-03-22 19:01:14 +0530

    Created Route_Table , IG, Security_Group

--- main.tf
+++ main.tf
@@ -1,28 +1,151 @@
-provider "aws" {}
-
-variable cidr_blocks {
-    description = "cidr blocks and name tags for vpc and subnets"
-    type = list(object({
-        cidr_block = string
-        name = string
-    }))
+provider "aws" {
+    region = "ap-south-1"
 }
 
+//Variables
+
+variable vpc_cidr_blocks {}
+variable subnet_cidr_blocks {}
 variable avail_zone {}
+variable "env_prefix" {}
+variable my_ip {}
+
+
+//VPC
 
 resource "aws_vpc" "myapp-vpc" {
-    cidr_block = var.cidr_blocks[0].cidr_block
+    cidr_block = var.vpc_cidr_blocks
     tags = {
-        Name: var.cidr_blocks[0].name
+        Name = "${var.env_prefix}-vpc"
     }
 }
+
+//Subnet
 
 resource "aws_subnet" "myapp-subnet-1" {
     vpc_id = aws_vpc.myapp-vpc.id
-    cidr_block = var.cidr_blocks[1].cidr_block
+    cidr_block = var.subnet_cidr_blocks
     availability_zone = var.avail_zone
     tags = {
-        Name: var.cidr_blocks[1].name
+      Name = "${var.env_prefix}-subnet-1"
     }
 }
+
+//Internet Gateway
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  tags = {
+    "Name" = "${var.env_prefix}-igw"
+  }
+}
+
+// Route Table
+
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  route   {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+ 
+  } 
+
+  tags = {
+    "Name" = "${var.env_prefix}-rtb"
+  }
+}
+
+// Attaching subnet to Route Table
+
+resource "aws_route_table_association" "a-rtb-subnet" {
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
+}
+
+//Attaching default route table
+/*
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+  route   {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+ 
+  } 
+
+  tags = {
+    "Name" = "${var.env_prefix}-main-rtb"
+  }
+}
+*/
+
+//Security Group
+
+resource "aws_security_group" "myapp-sg" {
+  name = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port = 22 
+    to_port = 22   
+    protocol = "tcp"
+    // cidr_blocks = [49.37.195.124/32] //myappaddress - if it's dynamic we can access it through variable
+    cidr_blocks = [var.my_ip]
+  }
+
+    ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0 
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    "Name" = "${var.env_prefix}-sg"
+  }
+}
+
+// Using exisiting default security group - (which created default when VPC created)
+
+/*
+resource "aws_default_security_group" "default-sg" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-default-sg"
+  }
+}
+*/
 
